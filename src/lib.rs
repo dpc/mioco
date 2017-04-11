1d56d5a94ae7c7fe00f3b61747bbddc5debcde35
Dawid Ciężarkiewicz <dpc@dpc.pw>
2017-04-10 21:52:09 -0700

    Progress

--- src/lib.rs
+++ src/lib.rs
@@ -12,7 +12,7 @@ extern crate slab;
 use slog::{Logger, Drain};
 
 use std::{mem, thread};
-use std::cell::UnsafeCell;
+use std::cell::{UnsafeCell, RefCell};
 
 use std::sync::{mpsc, Mutex};
 use std::sync::atomic::{AtomicUsize, Ordering};
@@ -43,64 +43,6 @@ fn miofib_logger() -> Logger {
 }
 // }}}
 
-// {{{ Fiber
-struct Fiber {
-    cur_context: context::Context,
-    stack: stack::ProtectedFixedSizeStack,
-    log : Logger,
-}
-
-impl Fiber {
-    fn new<F, T>(f: F, log : &Logger) -> Self
-        where F: Send + 'static + FnOnce() -> T,
-              T: Send + 'static
-    {
-        trace!(log, "spawning fiber");
-
-        let mut f = UnsafeCell::new(Some(
-            boxfnonce::SendBoxFnOnce::from(
-                move |mut t: context::Transfer| {
-                    f()
-                })
-        ));
-
-        extern "C" fn context_function(mut t: context::Transfer) -> ! {
-
-            let f = {
-                let cell : &mut UnsafeCell<Option<boxfnonce::SendBoxFnOnce<(context::Transfer,),()>>> = unsafe { mem::transmute(t.data) };
-                unsafe { (*cell.get()).take() }.unwrap()
-            };
-
-            printerrln!("Here2");
-            let t = t.context.resume(0);
-
-            f.call(t);
-            printerrln!("Here3");
-
-            unreachable!();
-        }
-
-        let stack = stack::ProtectedFixedSizeStack::default();
-
-        let context = context::Context::new(&stack, context_function);
-        let t = context.resume(&mut f as *mut _ as usize);
-        debug_assert!(unsafe { (&*f.get()) }.is_none());
-
-        trace!(log, "fiber created");
-        Fiber {
-            cur_context: t.context,
-            stack: stack,
-            log: log.clone(),
-        }
-    }
-
-    fn resume(&mut self) {
-    }
-}
-
-unsafe impl Send for Fiber {}
-// }}}
-
 // {{{ Miofib
 lazy_static! {
     static ref MIOFIB: Miofib = {
@@ -111,31 +53,47 @@ lazy_static! {
 struct Miofib {
     spawn_tx_i: AtomicUsize,
     loop_tx: Vec<LoopTx>,
-    loop_join: Vec<std::thread::JoinHandle<()>>,
-    log : Logger,
+    loop_join: Vec<thread::JoinHandle<()>>,
+    log: Logger,
+    polls: Vec<mio::Poll>,
 }
 
+impl Drop for Miofib {
+    fn drop(&mut self) {
+        self.loop_join
+            .drain(..)
+            .map(|join| { let _ = join.join(); })
+            .count();
+    }
+}
 
 
 impl Miofib {
     fn new() -> Self {
         let log = miofib_logger();
         info!(log, "Creating miofib instance");
-        let (txs, joins) = (0..1)
+        let (txs, mut joins_and_polls): (_, Vec<(_, _)>) = (0..1)
             .map(|i| {
                      let (tx, rx) = channel();
-                     let mut loop_ = Loop::new(i, rx, &log);
-                     let join = std::thread::spawn(move || loop_.run());
-                     (tx, join)
+                     let (mut loop_, mio_loop) = Loop::new(i, rx, &log);
+                     let join = thread::spawn(move || loop_.run());
+                     (tx, (join, mio_loop))
                  })
             .unzip();
+
+        let (joins, polls) = joins_and_polls.drain(..).unzip();
 
         Miofib {
             spawn_tx_i: AtomicUsize::new(0),
             loop_tx: txs,
             loop_join: joins,
+            polls: polls,
             log: log,
         }
+    }
+
+    fn poll(&self, i: usize) -> &mio::Poll {
+        &self.polls[i]
     }
 
     fn spawn<F, T>(&self, f: F)
@@ -152,7 +110,90 @@ impl Miofib {
 }
 // }}}
 
-// {{{ Channel
+// {{{ Fiber
+thread_local! {
+    // TODO: UnsafeCell is most probably redundant with RefCell here
+    pub static TL_CUR_TRANSFER: UnsafeCell<RefCell<Option<context::Transfer>>> = UnsafeCell::new(RefCell::new(None));
+}
+
+fn save_transfer(t: context::Transfer) {
+    TL_CUR_TRANSFER.with(|cur_t| {
+        let cur_transfer : &mut RefCell<Option<context::Transfer>> = unsafe { &mut *cur_t.get() } ;
+        let mut cur_transfer = cur_transfer.borrow_mut();
+        debug_assert!(cur_transfer.is_none());
+
+        *cur_transfer = Some(t);
+    })
+}
+
+fn pop_transfer() -> context::Transfer {
+    TL_CUR_TRANSFER.with(|cur_t| {
+        let cur_transfer : &mut RefCell<Option<context::Transfer>> = unsafe { &mut *cur_t.get() } ;
+        let mut cur_transfer = cur_transfer.borrow_mut();
+
+        cur_transfer.take().expect("pop_transfer")
+    })
+}
+
+struct Fiber {
+    cur_context: Option<context::Context>,
+    stack: stack::ProtectedFixedSizeStack,
+    log: Logger,
+}
+
+impl Fiber {
+    fn new<F, T>(f: F, log: &Logger) -> Self
+        where F: Send + 'static + FnOnce() -> T,
+              T: Send + 'static
+    {
+        trace!(log, "spawning fiber");
+
+        // Workaround for Box<FnOnce> not working
+        let mut f = UnsafeCell::new(Some(boxfnonce::SendBoxFnOnce::from(move || f())));
+
+        extern "C" fn context_function(t: context::Transfer) -> ! {
+
+            let f = {
+                let cell: &mut UnsafeCell<Option<boxfnonce::SendBoxFnOnce<(), ()>>> =
+                    unsafe { mem::transmute(t.data) };
+                unsafe { (*cell.get()).take() }.unwrap()
+            };
+
+            let t = t.context.resume(0);
+
+            save_transfer(t);
+
+            f.call();
+
+            pop_transfer().context.resume(1);
+
+            unreachable!();
+        }
+
+        let stack = stack::ProtectedFixedSizeStack::default();
+
+        let context = context::Context::new(&stack, context_function);
+        let t = context.resume(&mut f as *mut _ as usize);
+        debug_assert!(unsafe { (&*f.get()) }.is_none());
+
+        trace!(log, "fiber created");
+        Fiber {
+            cur_context: Some(t.context),
+            stack: stack,
+            log: log.clone(),
+        }
+    }
+
+    fn resume(&mut self) {
+        let t = self.cur_context.take().unwrap().resume(0);
+        self.cur_context = Some(t.context);
+    }
+}
+
+unsafe impl Send for Fiber {}
+// }}}
+
+// {{{ LoopChannel
 enum LoopMsg {
     Spawn(Fiber),
 }
@@ -172,7 +213,7 @@ impl LoopTx {
     }
     fn send(&self, msg: LoopMsg) {
         self.tx.lock().unwrap().send(msg).unwrap();
-        self.ctrl.set_readiness(mio::Ready::readable());
+        self.ctrl.set_readiness(mio::Ready::readable()).unwrap();
     }
 }
 
@@ -204,16 +245,16 @@ fn channel() -> (LoopTx, LoopRx) {
 // {{{ Loop
 /// Event loop on a given thread
 struct Loop {
-    poll: mio::Poll,
+    id: usize,
     rx: LoopRx,
-    fibers : Slab<Fiber>,
+    fibers: Slab<Fiber>,
     log: Logger,
 }
 
 impl Loop {
-    fn new(loop_id: usize, rx: LoopRx, log: &Logger) -> Self {
+    fn new(id: usize, rx: LoopRx, log: &Logger) -> (Self, mio::Poll) {
 
-        let log = log.new(o!("loop_id" => loop_id));
+        let log = log.new(o!("loop-id" => id));
 
         trace!(log, "creating loop");
         let poll = mio::Poll::new().unwrap();
@@ -221,14 +262,16 @@ impl Loop {
         poll.register(&rx.rx_registration,
                       mio::Token(0),
                       mio::Ready::readable(),
-                      mio::PollOpt::edge());
+                      mio::PollOpt::edge())
+            .unwrap();
 
-        Loop {
-            poll: poll,
-            rx: rx,
-            log: log,
-            fibers: Slab::with_capacity(1024),
-        }
+        (Loop {
+             id: id,
+             rx: rx,
+             log: log,
+             fibers: Slab::with_capacity(1024),
+         },
+         poll)
     }
 
     fn run(&mut self) {
@@ -236,13 +279,11 @@ impl Loop {
 
         loop {
             trace!(self.log, "poll");
-            let event_num = self.poll.poll(&mut events, None).unwrap();
+            let event_num = MIOFIB.poll(self.id).poll(&mut events, None).unwrap();
             trace!(self.log, "events"; "no" => event_num);
 
-
             for event in &events {
-                if event.token() == mio::Token(0) &&
-                    event.readiness().is_readable() {
+                if event.token() == mio::Token(0) && event.readiness().is_readable() {
                     trace!(self.log, "rx queue ready");
                     self.poll_queue();
                 } else {
@@ -257,17 +298,92 @@ impl Loop {
             match msg {
                 LoopMsg::Spawn(fiber) => {
                     match self.fibers.insert(fiber) {
-                        Ok( fib_i) => {
+                        Ok(fib_i) => {
+                            trace!(self.log, "resuming"; "fiber-id" => fib_i);
                             self.fibers[fib_i].resume();
                         }
                         Err(_fiber) => panic!("Run out of slab"),
                     }
-                },
+                }
 
             }
         }
     }
 }
+// }}}
+
+// {{{ Evented
+thread_local! {
+    // TODO: UnsafeCell is most probably redundant with RefCell here
+    pub static TL_FIBER_ID: UnsafeCell<usize> =
+        UnsafeCell::new(-1isize as usize);
+    pub static TL_LOOP_ID: UnsafeCell<usize> =
+        UnsafeCell::new(-1isize as usize);
+}
+
+
+
+pub trait Evented {
+    fn notify_on(&mut self, interest: mio::Ready);
+}
+
+//impl<T> Evented for T where T: mio::Evented {}
+
+struct AsyncIO<T>
+    where T: mio::Evented
+{
+    io: T,
+    registered_on: Option<(usize, usize, mio::Ready)>,
+}
+
+impl<T> Evented for AsyncIO<T>
+    where T: mio::Evented
+{
+    // Handle out-of loop condition (cur_loop == -1?)
+    fn notify_on(&mut self, interest: mio::Ready) {
+        let cur_fiber = TL_FIBER_ID.with(|id| unsafe { *id.get() });
+        let cur_loop = TL_LOOP_ID.with(|id| unsafe { *id.get() });
+        match self.registered_on {
+            Some((my_loop, my_fiber, my_readiness)) => {
+                if cur_loop == my_loop {
+                    if (cur_fiber, interest) != (my_fiber, my_readiness) {
+                        MIOFIB
+                            .poll(cur_loop)
+                            .reregister(&self.io,
+                                        mio::Token(my_fiber),
+                                        interest,
+                                        mio::PollOpt::oneshot())
+                            .unwrap();
+                        self.registered_on = Some((cur_loop, cur_fiber, interest));
+                    }
+                } else {
+                    MIOFIB.poll(my_loop).deregister(&self.io).unwrap();
+                    MIOFIB
+                        .poll(cur_loop)
+                        .register(&self.io,
+                                  mio::Token(cur_fiber),
+                                  interest,
+                                  mio::PollOpt::oneshot())
+                        .unwrap();
+                    self.registered_on = Some((cur_loop, cur_fiber, interest));
+                }
+            }
+            None => {
+                MIOFIB
+                    .poll(cur_loop)
+                    .register(&self.io,
+                              mio::Token(cur_fiber),
+                              interest,
+                              mio::PollOpt::oneshot())
+                    .unwrap();
+                self.registered_on = Some((cur_loop, cur_fiber, interest));
+            }
+        }
+
+    }
+}
+
+
 // }}}
 
 // {{{ API
@@ -278,6 +394,11 @@ pub fn spawn<F, T>(f: F)
           T: Send + 'static
 {
     MIOFIB.spawn(f)
+}
+
+pub fn yield_now() {
+    let t = pop_transfer().context.resume(0);
+    save_transfer(t);
 }
 // }}}
 
@@ -290,15 +411,11 @@ mod tests {
     fn it_works() {
         spawn(|| {
                   printerrln!("It works");
-                  assert_eq!(1, 2);
-
               });
 
         thread::spawn(|| thread::sleep_ms(3000)).join().unwrap();
         spawn(|| {
-                  printerrln!("It works");
-                  assert_eq!(1, 2);
-
+                  printerrln!("It works2");
               });
 
 
