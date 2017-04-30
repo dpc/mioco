619ea236e45e5a66fe1dc898d3732506eea3908c
tekjar <k.teza1@gmail.com>
2017-05-01 00:03:14 +0530

    remove sleeps from mpsc tests & use channel for synchronization

--- tests/mpsc.rs
+++ tests/mpsc.rs
@@ -5,12 +5,7 @@ use std::thread;
 use std::sync::atomic::{AtomicUsize, Ordering};
 use std::sync::Arc;
 
-// NOTE: All the tests are using single MIOCO instance and
-// the number of messages that reciver receives varies
-// across machines. so the tests here aren't very scalable.
-// on the other hand, the sleep times are generous and
-// receiver should ideally receive all the messages. anything
-// otherwise might help identify performance issues.
+// NOTE: All the tests are using single MIOCO instance
 
 #[test]
 fn tx_rx_outside_mioco() {
@@ -30,30 +25,26 @@ fn tx_rx_outside_mioco() {
 #[test]
 fn tx_outside_rx_inside_mioco() {
     let (tx, rx) = mpsc::channel::<usize>();
-    // use count to check if all the messages are
-    // received at receiver end
-    let count = Arc::new(AtomicUsize::new(0));
+    let (sync_tx, sync_rx) = mpsc::channel();
 
     for i in 0..10 {
         let _ = tx.send(i);
     }
 
-    let count_clone = count.clone();
     mioco::spawn(move || {
         for i in 0..10 {
             assert_eq!(i, rx.recv().unwrap());
-            count_clone.store(i, Ordering::SeqCst);
         }
+        let _ = sync_tx.send(true);
     });
 
-    thread::sleep_ms(3000);
-    assert_eq!(9, count.load(Ordering::SeqCst));
+    assert_eq!(true, sync_rx.recv().unwrap());
 }
 
 #[test]
 fn tx_inside_rx_inside_mioco() {
     let (tx, rx) = mpsc::channel::<usize>();
-    let count = Arc::new(AtomicUsize::new(0));
+    let (sync_tx, sync_rx) = mpsc::channel();
     
     mioco::spawn(move ||{
         for i in 0..10 {
@@ -61,22 +52,20 @@ fn tx_inside_rx_inside_mioco() {
         }
     });
 
-    let count_clone = count.clone();
     mioco::spawn(move || {
         for i in 0..10 {
             assert_eq!(i, rx.recv().unwrap());
-            count_clone.store(i, Ordering::SeqCst);
         }
+        let _ = sync_tx.send(true);
     });
 
-    thread::sleep_ms(3000);
-    assert_eq!(9, count.load(Ordering::SeqCst));
+    assert_eq!(true, sync_rx.recv().unwrap());
 }
 
 #[test]
 fn sync_tx_rx_outside_mioco() {
     let (tx, rx) = mpsc::sync_channel::<usize>(5);
-    let count = Arc::new(AtomicUsize::new(0));
+    let (sync_tx, sync_rx) = mpsc::channel();
 
     thread::spawn(move || for i in 0..10 {
                       let _ = tx.send(i);
@@ -84,20 +73,20 @@ fn sync_tx_rx_outside_mioco() {
 
     thread::sleep_ms(1000);
 
-    let count_clone = count.clone();
-    for i in 0..10 {
-        assert_eq!(i, rx.recv().unwrap());
-        count_clone.store(i, Ordering::SeqCst);
-    }
+    mioco::spawn(move || {
+        for i in 0..10 {
+            assert_eq!(i, rx.recv().unwrap());
+        }
+        let _ = sync_tx.send(true);
+    });
 
-    thread::sleep_ms(3000);
-    assert_eq!(9, count.load(Ordering::SeqCst));
+    assert_eq!(true, sync_rx.recv().unwrap());
 }
 
 #[test]
 fn sync_tx_outside_rx_inside_mioco() {
     let (tx, rx) = mpsc::sync_channel::<usize>(5);
-    let count = Arc::new(AtomicUsize::new(0));
+    let (sync_tx, sync_rx) = mpsc::channel();
 
     thread::spawn(move || {
         for i in 0..10 {
@@ -105,22 +94,20 @@ fn sync_tx_outside_rx_inside_mioco() {
         }
     });
 
-    let count_clone = count.clone();
     mioco::spawn(move || {
         for i in 0..10 {
             assert_eq!(i, rx.recv().unwrap());
-            count_clone.store(i, Ordering::SeqCst);
         }
+        let _ = sync_tx.send(true);
     });
 
-    thread::sleep_ms(1000);
-    assert_eq!(9, count.load(Ordering::SeqCst));
+    assert_eq!(true, sync_rx.recv().unwrap());
 }
 
 #[test]
 fn sync_tx_inside_rx_inside_mioco() {
     let (tx, rx) = mpsc::sync_channel::<usize>(5);
-    let count = Arc::new(AtomicUsize::new(0));
+    let (sync_tx, sync_rx) = mpsc::channel();
 
     mioco::spawn(move || {
         for i in 0..10 {
@@ -130,14 +117,12 @@ fn sync_tx_inside_rx_inside_mioco() {
 
     thread::sleep_ms(1000);
 
-    let count_clone = count.clone();
     mioco::spawn(move || {
         for i in 0..10 {
             assert_eq!(i, rx.recv().unwrap());
-            count_clone.store(i, Ordering::SeqCst);
         }
+        let _ = sync_tx.send(true);
     });
 
-    thread::sleep_ms(3000);
-    assert_eq!(9, count.load(Ordering::SeqCst));
+    assert_eq!(true, sync_rx.recv().unwrap());
 }
